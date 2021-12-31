ae41775484fa6bc5f4801cd8709da07d2039e445
Francis Rousseau <frr@mybingli.com>
2021-12-31 09:06:57 +0100

    update whitelist bot file

--- main.py
+++ main.py
@@ -4,31 +4,57 @@ from discord.ext import commands
 import discord
 
 TOKEN = os.getenv('DISCORD_TOKEN')
+
 bot = commands.Bot(command_prefix='!')
 client = discord.Client()
-
-
 @client.event
 async def on_message(message):
     if message.guild:
-        if not str('923579001010794527') in str(message.author.id):
+        if not str('923579001010794527') in str(message.author.id) \
+                and (message.content == '>>help' \
+                or message.content == '>>info' \
+                or message.content == '>>check' or\
+                message.content == '>>set'):
             print(message.author.id)
             await message.channel.send(f"<@{message.author.id}>, please send me a private message...")
 
     else:
-        if message.content == '>>help':
-            await message.channel.send(getHelpMessage())
-        elif message.content == '>>info':
-            await message.channel.send(getInfoMessage())
-        elif message.content == '>>check':
-            result = readDatabase('check', message.author.id, message.content)
-            if len(result) > 0:
-                await message.channel.send('Your address is set: ' + str(result[0][0]))
-            else:
-                await message.channel.send('No address set yet.')
-        elif str('>>set') in message.content:
-            readDatabase('set', message.author.id, message.content.replace('>>set ', ''))
-            await message.channel.send('Address is set! Use >>check to verify!>')
+        if not str('923579001010794527') in str(message.author.id):
+            if message.content == '>>help':
+                await message.channel.send(getHelpMessage())
+            elif message.content == '>>info':
+                await message.channel.send(getInfoMessage())
+            elif message.content == '>>check':
+                result = readDatabase('check', message.author.id, message.content)
+                if len(result) > 0:
+                    await message.channel.send('Your address is set: ' + str(result[0][0]))
+                else:
+                    await message.channel.send('No address set yet.')
+            elif str('>>set') in message.content:
+                result = readDatabase('check', message.author.id, message.content)
+                token = message.content.replace('>>set ', '')
+                if len(result) == 1:
+                    await message.channel.send('Sorry! There is already set an address for this user.')
+                else:
+                    if len(token) > 25:
+                        readDatabase('set', message.author.id, token)
+                        await message.channel.send('Address is set! Use **>>check** to verify!')
+                    else:
+                        await message.channel.send('Not a valid token address!')
+
+            elif str('>>update') in message.content:
+                result = readDatabase('check', message.author.id, message.content)
+                token = message.content.replace('>>update ', '')
+                if len(result) == 0:
+                    await message.channel.send('Sorry! There is no address for this account yet, use **>>set**')
+                else:
+                    if len(token) > 25:
+                        readDatabase('update', message.author.id, token)
+
+
+                    else:
+                        await message.channel.send('Not a valid token address!')
+
 
 
 def getHelpMessage():
@@ -56,7 +82,11 @@ async def on_ready():
 def readDatabase(type, value, value2):
     global connection
     try:
-        connection = psycopg2.connect('Good try :)')
+        connection = psycopg2.connect(user=os.getenv('USER'),
+                                      password=os.getenv('PASSWORD'),
+                                      host=os.getenv('HOST'),
+                                      port=5432,
+                                      database=os.getenv('DATABASE'))
         cursor = connection.cursor()
 
         result = ''
@@ -65,12 +95,20 @@ def readDatabase(type, value, value2):
             query = 'SELECT address, name FROM public.whitelists w where name = \'' + str(value) + '\''
             cursor.execute(query)
             result = cursor.fetchall()
-        elif type == 'set':
+
+        if type == 'set':
             postgres_insert_query = """ INSERT INTO public.whitelists(address, name) VALUES (%s, %s)"""
             record_to_insert = (value2, value)
             cursor.execute(postgres_insert_query, record_to_insert)
             connection.commit()
             result = cursor.rowcount
+        if type == 'update':
+            postgres_insert_query = """ UPDATE public.whitelists SET address=(%s), name=(%s) WHERE name = (%s);"""
+            record_to_insert = (value2, value, str(value))
+            cursor.execute(postgres_insert_query, record_to_insert)
+            connection.commit()
+            result = cursor.rowcount
+
 
         print('res', result)
         return result
