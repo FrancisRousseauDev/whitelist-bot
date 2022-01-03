518d13e885da9cd755dc7ed8125af19e0743413f
Francis Rousseau <frr@mybingli.com>
2022-01-03 09:26:46 +0100

    added tools

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
-    print(message.channel)
+    print(message.channel == '🛠add-whitelist')
     if message.guild:
         if not str('923579001010794527') in str(message.author.id):
             if message.content == '>>help':
